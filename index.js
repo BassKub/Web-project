0fb15a380215896935f552f493bbe70f3f512ca1
Suparoek Grudsorn <gr.Suparoek@hotmail.com>
2022-12-20 20:22:22 +0700

    add error page

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ const app = express()
 app.get('/', (req, res) =>{
     res.send("Welcome to my first work with nodejs")
 })
+
+app.get('/product', (req, res) =>{
+    res.sendFile(__dirname + '/Zerotwo.html')
+})
+
+app.get('*', (req, res) =>{
+    res.sendFile(__dirname + '/Error.html')
+})
+
 app.listen(3000, () => {
     console.log("Start Server at Port [3000]")
 })
