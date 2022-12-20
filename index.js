b2795692e9fb732cd8f44cc239ebe81b7151609b
Suparoek Grudsorn <gr.Suparoek@hotmail.com>
2022-12-20 20:35:32 +0700

    change style and add home

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express()
 
 app.get('/', (req, res) =>{
-    res.send("Welcome to my first work with nodejs")
+    res.sendFile(__dirname + '/home.html')
 })
 
 app.get('/product', (req, res) =>{
